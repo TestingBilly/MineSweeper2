2a7e2f1fce56793a3c165bc8cbff5f8dccb0bce1
Billy Barker <BBarker@softwareinstitue.com>
2021-08-24 10:48:47 +0100

    Added Testing 3

--- src/test/java/testThree.java
+++ src/test/java/testThree.java
@@ -1,6 +1,16 @@
 package minesweeper;
 
-public class testThree
-{
+import org.junit.jupiter.api.DisplayName;
+import org.testng.annotations.Test;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class testThree {
+    @Test
+    @DisplayName("Third Test")
+    public void testIsUncovered() {
+        Block blockTestThree = new Block(3, 3);
+        blockTestThree.setUncovered(true);
+        assertTrue(blockTestThree.isUncovered(), "This is uncovered");
+    }
 }
